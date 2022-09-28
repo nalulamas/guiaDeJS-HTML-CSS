04ce0728edbfff0ce7cc579c30848d7f92901f49
Ana Luiza <67602155+nalulamas@users.noreply.github.com>
2022-09-28 17:32:23 -0300

    Add files via upload

---
+++ aula11/ex0011.js
@@ -0,0 +1,10 @@
+console.log('Testando o console')
+
+let vel = 60
+console.log(`A velocidade média do seu carro é ${vel}km/h`)
+if( vel > 60 ){
+  console.log('Voce ultrapassou a velocidade permitida. MULTADO!')
+} else {
+  console.log('Velocidade permitida. Continue prudente ao dirijir.')
+}
+console.log('Dirija com cuidado')
---
+++ aula11/ex0012.js
@@ -0,0 +1,8 @@
+let país = 'EUA'
+if( país == 'BR') {
+  console.log('Voce é brasileiro')
+} else if( país == 'EUA') {
+  console.log('Voce é dos Estados unidos')
+} else {
+  console.log('País nao cadastrado')
+}
